bd3fdab4b193ea46ce44ace3a5d43c64db788ee8
Sai Santosh Pal <saisantoshpal2010@gmail.com>
2026-01-29 22:17:25 +0530

    player sprite now displayed - working on player animations

--- code/player.py
+++ code/player.py
@@ -1,17 +1,32 @@
 import pygame
 from settings import *
+from support import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
 
-        self.image = pygame.Surface((32,64))
-        self.image.fill('green')
+        self.import_assets()
+        self.status = 'down_idle'
+        self.frame_index = 0
+
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center = pos)
 
         self.direction = pygame.math.Vector2()
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
+
+    def import_assets(self):
+        self.animations = {'up': [],'down': [],'left': [],'right': [],
+                           'right_idle': [],'left_idle': [],'up_idle': [],'down_idle': [],
+                           'right_hoe': [],'left_hoe': [],'up_hoe': [],'down_hoe': [],
+                           'right_axe': [],'left_axe': [],'up_axe': [],'down_axe': [],
+                           'right_water': [],'left_water': [],'up_water': [],'down_water': []}
+
+        for animation in self.animations.keys():
+            full_path = './graphics/character/' + animation
+            self.animations[animation] = import_folder(full_path)
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -31,8 +46,14 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = 0
 
     def move(self,dt):
-        self.pos += self.direction * self.speed * dt
-        self.rect.center = self.pos
+        if self.direction.magnitude() > 0:
+            self.direction = self.direction.normalize()
+
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
 
     def update(self, dt):
         self.input()
