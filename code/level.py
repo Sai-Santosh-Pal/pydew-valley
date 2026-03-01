6b46b508cc103e6e4e34affb3ed3c957726e26e7
Sai Santosh Pal <saisantoshpal2010@gmail.com>
2026-03-01 20:01:39 +0530

    imported all elements and added realistic layering

--- code/level.py
+++ code/level.py
@@ -2,8 +2,9 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic
+from sprites import Generic, Water, WildFlower, Tree
 from pytmx.util_pygame import load_pygame
+from support import *
 
 class Level:
 	def __init__(self):
@@ -25,7 +26,20 @@ class Level:
 
 		for layer in ['HouseWalls', 'HouseFurnitureTop']:
 			for x, y, surf in tmx_data.get_layer_by_name(layer).tiles():
-				Generic((x * TILE_SIZE,y * TILE_SIZE), surf, self.all_sprites, LAYERS['main'])
+				Generic((x * TILE_SIZE,y * TILE_SIZE), surf, self.all_sprites)
+
+		for x, y, surf in tmx_data.get_layer_by_name('Fence').tiles():
+			Generic((x * TILE_SIZE, y*TILE_SIZE), surf, self.all_sprites)
+
+		water_frames = import_folder('./graphics/water')
+		for x, y, surf in tmx_data.get_layer_by_name('Water').tiles():
+			Water((x * TILE_SIZE, y*TILE_SIZE), water_frames, self.all_sprites)
+
+		for obj in tmx_data.get_layer_by_name('Trees'):
+			Tree((obj.x, obj.y), obj.image, self.all_sprites, obj.name)		
+
+		for obj in tmx_data.get_layer_by_name('Decoration'):
+			WildFlower((obj.x, obj.y), obj.image, self.all_sprites)		
 
 		self.player = Player((640,360), self.all_sprites)
 		Generic(
@@ -52,7 +66,7 @@ class CameraGroup(pygame.sprite.Group):
 		self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
 
 		for layer in LAYERS.values():
-			for sprite in self.sprites():
+			for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
 				if sprite.z == layer:
 					offset_rect = sprite.rect.copy()
 					offset_rect.center -= self.offset
