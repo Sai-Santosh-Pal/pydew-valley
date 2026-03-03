68f3355c9235c9b3d141fb55cec79dcd04fc777b
Sai Santosh Pal <saisantoshpal2010@gmail.com>
2026-03-03 18:39:07 +0530

    completed collision logic

--- code/level.py
+++ code/level.py
@@ -42,7 +42,12 @@ class Level:
 		for obj in tmx_data.get_layer_by_name('Decoration'):
 			WildFlower((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites])		
 
-		self.player = Player((1500,360), self.all_sprites, self.collision_sprites)
+		for x, y, surf in tmx_data.get_layer_by_name('Collision').tiles():
+			Generic((x * TILE_SIZE,y * TILE_SIZE), pygame.Surface((TILE_SIZE, TILE_SIZE)), self.collision_sprites)
+
+		for obj in tmx_data.get_layer_by_name('Player'):
+			if obj.name == 'Start':
+				self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
 		Generic(
 			pos = (0,0),
 			surf = pygame.image.load('graphics/world/ground.png').convert_alpha(),
